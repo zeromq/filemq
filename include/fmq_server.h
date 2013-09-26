f0c2ad1e0d840daa1d25fac5fbf87d1c9c6690e3
Momo the Monster <surya@mmmlabs.com>
2013-09-26 11:28:02 -0700

    Define strcasecmp as _stricmp for MSVC

--- include/fmq_server.h
+++ include/fmq_server.h
@@ -22,6 +22,7 @@
     along with this program. If not, see http://www.gnu.org/licenses/.      
     =========================================================================
 */
+#define strcasecmp _stricmp
 
 #ifndef __FMQ_SERVER_H_INCLUDED__
 #define __FMQ_SERVER_H_INCLUDED__
